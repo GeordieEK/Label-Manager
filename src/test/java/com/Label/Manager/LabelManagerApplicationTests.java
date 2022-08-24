6d6f62fd84a8193c279249d6f9226b21b97d10b2
GeordieEK <geordieek@gmail.com>
2022-08-24 22:57:43 +1000

    Initial commit

---
+++ src/test/java/com/Label/Manager/LabelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Label.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
