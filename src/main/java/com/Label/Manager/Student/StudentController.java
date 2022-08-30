d01ffd7d55a963619337a02966f3ea57105d457b
GeordieEK <geordieek@gmail.com>
2022-08-30 17:31:25 +1000

    Learning Spring handling of basic GET and POST requests using service and controller classes in an MVC type model

--- src/main/java/com/Label/Manager/Student/StudentController.java
+++ src/main/java/com/Label/Manager/Student/StudentController.java
@@ -1,27 +1,39 @@
 package com.Label.Manager.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+//Tells Spring this is a REST controller
 @RestController
-@RequestMapping(path = "api/v1/student")
+// @RequestMapping signifies the type of request and its url path, it is used at class level
+@RequestMapping(path = "api/v1")
 public class StudentController {
 
-    private final StudentService studentService;
-
+    // @Autowired annotation marks this as something that needs dependency injection
+    // This means when your method runs, there's an instance of the dependent object available
     @Autowired
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
+    private StudentService studentService;
+
+    // @GetMapping is @RequestMapping combined with .GET and is used at method level
+    @GetMapping(path = "/student")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
     }
 
-    @GetMapping
-    public List<Student> getStudents() {
-        return studentService.getStudents();
+    @GetMapping(path = "/student/{id}")
+    // @PathVariable tells spring to pull the variable from the url path portion in {}
+    // They will automatically match if the variable in the path and code are the same, or it can be passed as a parameter to @PathVariable
+    public Student getStudent(@PathVariable long id) {
+        return studentService.getStudent(id);
+    }
+
+    // @RequestBody allows for the .json payload of the request body to be converted to a student instance
+    @PostMapping(path = "/student")
+    public void addStudent(@RequestBody Student student) {
+        studentService.addStudent(student);
     }
 }
--- src/main/java/com/Label/Manager/Student/StudentService.java
+++ src/main/java/com/Label/Manager/Student/StudentService.java
@@ -1,24 +1,42 @@
 package com.Label.Manager.Student;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class StudentService {
 
-    public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+    private ArrayList<Student> studentList = new ArrayList<>(Arrays.asList(
+            new Student(
+                    1L,
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            ),
+            new Student(
+                    2L,
+                    "Geordie",
+                    "geordie.jamal@gmail.com",
+                    LocalDate.of(2003, Month.SEPTEMBER, 5),
+                    21
+            )
+    ));
+
+    public List<Student> getAllStudents() {
+        return studentList;
+    }
+
+    public Student getStudent(long id) {
+        return studentList.stream().filter(s -> s.getId().equals(id)).findFirst().get();
+    }
+
+    public void addStudent(Student student) {
+        studentList.add(student);
     }
 }
